4cf6644f19ce76cc9629fed62f364a28b91d9793
awe <WiRai@users.noreply.github.com>
2016-09-09 20:47:18 +0200

    better tests

--- __tests__/functions_test.js
+++ __tests__/functions_test.js
@@ -2,38 +2,34 @@ jest.unmock('fs-extra');
 jest.unmock('path');
 jest.unmock('os');
 jest.unmock('nunjucks');
+jest.unmock('superimp');
 jest.unmock('../lib/tasks');
 jest.unmock('../lib/featureman');
+jest.unmock('../lib/functions');
 
 const fs = require('fs-extra');
 const path = require('path');
 const os = require('os');
-const getFeatureList = require('../lib/featureman').getFeatureList;
-const getFeatures = require('../lib/featureman').getFeatures;
 const tasks = require('../lib/tasks');
 
 const skelPath = path.join(__dirname, '../.skel');
 
-describe('getFeatureList', () => {
-  it('Returns list of the names of active features', () => {
-    expect(getFeatureList()).toEqual([]);
-    process.env.PRODUCT_DIR = path.join(os.tmpdir(), '_ginjs-test-product');
-    fs.copySync(path.join(skelPath, 'product'), process.env.PRODUCT_DIR);
-    expect(getFeatureList()).toEqual(['gap']);
-    fs.removeSync(process.env.PRODUCT_DIR);
-    delete process.env.PRODUCT_DIR;
-  });
-});
-
-describe('getFeatures', () => {
-  it('Returns list of active features', () => {
+describe('Function module', () => {
+  it('Functionholder to introduce and refine functions with superimp', () => {
     process.env.PRODUCTLINE_DIR = path.join(os.tmpdir(), '_ginjs-test-productline');
     process.chdir(os.tmpdir());
     tasks.createProductLine('_ginjs-test-productline');
     process.env.PRODUCT_DIR = path.join(process.env.PRODUCTLINE_DIR, 'products', 'default');
     const gap = require('gap');
-    const featureList = getFeatures();
-    expect(gap).toBe(featureList[0]);
+    gap.composables.functions.introduce_test = () => 'i introduced a function';
+    gap.composables.functions.refine_deriveProduct = (original) => () => {
+      const product = original();
+      product.refined = true;
+      return product;
+    };
+    const functions = require('../lib/functions');
+    expect(functions.test()).toBe('i introduced a function');
+    expect(functions.deriveProduct().refined).toBe(true);
     fs.removeSync(process.env.PRODUCTLINE_DIR);
     delete process.env.PRODUCTLINE_DIR;
     delete process.env.PRODUCT_DIR;
