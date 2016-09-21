f8b15bb496a96cadc0c7f9552a8a176f7e51bbe9
awe <WiRai@users.noreply.github.com>
2016-09-21 19:15:52 +0200

    tests for settings, fix typo

--- src/settings.test.js
+++ src/settings.test.js
@@ -6,8 +6,8 @@ const tasks = require('../lib/tasks');
 
 const skelPath = path.join(__dirname, '../.skel');
 
-describe('Function module', () => {
-  it('Functionholder to introduce and refine functions with superimp', () => {
+describe('Settings module', () => {
+  it('Settingsholder to introduce and refine settings with superimp', () => {
     process.env.PRODUCTLINE_DIR = path.join(os.tmpdir(), '_ginjs-test-productline');
     process.env.PATH = `${process.env.PRODUCTLINE_DIR}/node_modules/.bin:${process.env.PATH}`;
     process.env.NODE_PATH = `${process.env.PRODUCTLINE_DIR}/features:${process.env.PRODUCTLINE_DIR}/node_modules:${process.env.NODE_PATH}`;
@@ -16,22 +16,21 @@ describe('Function module', () => {
     process.chdir(os.tmpdir());
     tasks.createProductLine('_ginjs-test-productline');
     process.env.PRODUCT_DIR = path.join(process.env.PRODUCTLINE_DIR, 'products', 'default');
-    /*
-     * Reset function module because tasks.createProductLine forces composition
-     * of functions module, but we change the features later dynamically.
-     * To get those features composed we need this pattern...
-     */
-    delete require.cache[require.resolve('../lib/functions')];
+    tasks.createFeature('gap2');
     const gap = require('gap');
-    gap.composables.functions.introduce_test = () => 'i introduced a function';
-    gap.composables.functions.refine_deriveProduct = (original) => () => {
-      const product = original();
-      product.refined = true;
-      return product;
-    };
-    const functions = require('../lib/functions');
-    expect(functions.test() === 'i introduced a function').to.be.true;
-    expect(functions.deriveProduct().refined).to.be.true;
+    gap.composables.settings = {};
+    gap.composables.settings.introduce_mySettings = true;
+    gap.composables.settings.introduce_mySettings2 = false;
+    const gap2 = require('gap2');
+    gap2.composables.settings = {};
+    gap2.composables.settings.refine_mySettings2 = () => true;
+    fs.writeJSONSync(path.join(process.env.PRODUCT_DIR, 'features.json'), [
+      'gap',
+      'gap2',
+    ]);
+    const settings = require('../lib/settings');
+    expect(settings.mySettings).to.be.true;
+    expect(settings.mySettings2).to.be.true;
     fs.removeSync(process.env.PRODUCTLINE_DIR);
     delete process.env.PRODUCTLINE_DIR;
     delete process.env.PRODUCT_DIR;
