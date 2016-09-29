81c6ea6b09adb1bcdbe16c638dc4286ddebdb6a6
awe <WiRai@users.noreply.github.com>
2016-09-29 23:36:08 +0200

    test for exposed settings

--- src/settings.test.js
+++ src/settings.test.js
@@ -28,6 +28,9 @@ describe('Settings module', () => {
       'gap',
       'gap2',
     ]);
+    // Because import of product forces composition of settings an we change gap and gap2 hot:
+    delete require.cache[require.resolve('ginjs')];
+    delete require.cache[require.resolve('ginjs/lib/settings')];    
     const settings = require('../lib/settings');
     expect(settings.mySettings).to.be.true;
     expect(settings.mySettings2).to.be.true;
