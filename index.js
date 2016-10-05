334041ca1fb58ebe62c4e23bf671cf021d740242
awe <WiRai@users.noreply.github.com>
2016-10-06 01:02:59 +0200

    index now in lib

--- index.js
+++
@@ -1,9 +0,0 @@
-module.exports.settings = require('./lib/settings');
-module.exports.functions = require('./lib/functions');
-module.exports.tasks = require('./lib/tasks');
-module.exports.featureman = require('./lib/featureman');
-
-if (process.env.PRODUCT_DIR) {
-  module.exports.context = require('./lib/context');
-  module.exports.product = require('./lib/product');
-}
